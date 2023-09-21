e35dd27b0bffad6119601d12afe5b5434cbe176f
Brito <jmbmaluno@gmail.com>
2023-09-21 10:31:22 -0300

    primeira versao

---
+++ main.py
@@ -0,0 +1,70 @@
+class Vetor():
+    def __init__(self, x , y):
+        self.x = x
+        self.y = y
+    
+    def __sub__(self, v):
+        return Vetor(self.x - v.x, self.y - v.y)
+    
+    def __add__(self, v):
+        return Vetor(self.x + v.x, self.y + v.y)
+    
+    def __mul__(self, k):
+        return Vetor(self.x * k, self.y * k)
+
+    def __str__(self):
+        return "[" + str(self.x) + ", " + str(self.y) + "]"
+    
+
+
+class Personagem:
+    def __init__(self, origem, velocidade, direcao = None, alvo = None):
+        self.origem = origem
+        self.velocidade = velocidade
+        self.ponto_atual = origem
+        self.alvo = alvo
+        self.direcao = direcao
+
+        if alvo != None:
+            self.direcao = alvo.ponto_atual - self.ponto_atual
+    
+    def __str__(self):
+        
+        return "Origem: " + str(self.origem) + "\nVelocidade: " +  str(self.velocidade) + "\nDireção: " +  str(self.direcao)
+
+    def atualizar_direcao(self):
+        if self.alvo != None:
+            self.direcao = self.alvo.ponto_atual - self.ponto_atual
+    
+    def andar(self):
+        self.atualizar_direcao()
+        self.ponto_atual = self.ponto_atual + (self.direcao * self.velocidade)
+
+
+
+
+
+rato = Personagem(Vetor(0,0), 1, direcao = Vetor(1,0))
+gato = Personagem(Vetor(0,1), 1, alvo = rato)
+
+
+print("rato: ", rato.ponto_atual)
+print("gato: ", gato.ponto_atual)
+
+rato.andar()
+gato.andar()
+
+print("\nrato: ", rato.ponto_atual)
+print("gato: ", gato.ponto_atual)
+
+rato.andar()
+gato.andar()
+
+print("\nrato: ", rato.ponto_atual)
+print("gato: ", gato.ponto_atual)
+
+rato.andar()
+gato.andar()
+
+print("\nrato: ", rato.ponto_atual)
+print("gato: ", gato.ponto_atual)
